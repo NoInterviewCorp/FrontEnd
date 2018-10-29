f792496c4aed0b3443ec5afcf9430dfb108e9513
soumya-ramteke <soumya.r@cgi.com>
2018-10-29 16:16:09 +0530

    added header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 import { AngularModule } from "./angular/angular.module";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LearnerComponent } from './learner/learner.component';
+import { HeaderComponent } from './learner/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LearnerComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
