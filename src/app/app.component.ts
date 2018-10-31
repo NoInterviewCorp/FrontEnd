2f1b450d834a78b5fdb714977646827c22729562
Soham Sahare <sohamsahare@gmail.com>
2018-10-31 11:14:57 +0530

    Moved Navbar code to component ->navbar changes on log in -> fixed sticky navbar going behind cards issue ->added background color to learning plan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,4 @@
-import { Component } from '@angular/core';
-import {
-  AuthService,
-  FacebookLoginProvider,
-  GoogleLoginProvider
-} from 'angular-6-social-login';
-import { MatDialog } from '@angular/material';
-import { SignInComponent } from './sign-in/sign-in.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { Router } from '@angular/router';
+import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -17,38 +8,9 @@ export class AppComponent {
   hasSignedIn = false;
   content = [1, 2, 3, 4]
   title = 'PotentiOMeter';
-  navbarOpen = false;
-  constructor(private socialAuthService: AuthService, public dialog: MatDialog, private router: Router) { }
-  toggleNavbar() {
-    this.navbarOpen = !this.navbarOpen;
+  handleEvent(event){
+    console.log(event);
+    this.hasSignedIn  = event;
   }
-  openDialog(isSignIn) {
-    let dialogRef = this.dialog.open(SignInComponent, { panelClass: 'custom-dialog-container', data:{isSignIn:isSignIn}} );
-    dialogRef.afterClosed().subscribe(result => {
-       this.signIn();
-    });
-  }
-  signIn() {
-    this.hasSignedIn = !this.hasSignedIn;
-    this.router.navigate(['profile'])
-  }
-  public socialSignIn(socialPlatform: string) {
-    let socialPlatformProvider;
-    if (socialPlatform == "facebook") {
-      socialPlatformProvider = FacebookLoginProvider.PROVIDER_ID;
-    }
-    else if (socialPlatform == "google") {
-      socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
-    }
-
-
-    this.socialAuthService.signIn(socialPlatformProvider).then(
-      (userData) => {
-        console.log(socialPlatform + " sign in data : ", userData);
-        // Now sign-in with userData
-        // ...
-
-      }
-    );
-  }
+  constructor() { }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { AuthService, FacebookLoginProvider, GoogleLoginProvider } from 'angular-6-social-login';
+import { MatDialog } from '@angular/material';
+import { Router } from '@angular/router';
+import { SignInComponent } from '../sign-in/sign-in.component';
 
 @Component({
   selector: 'app-navbar',
@@ -10,11 +11,41 @@ import { map } from 'rxjs/operators';
 })
 export class NavbarComponent {
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches)
+  @Output() onSignIn = new EventEmitter();
+  hasSignedIn:boolean = false;
+  navbarOpen = false;
+  constructor(private socialAuthService: AuthService, public dialog: MatDialog, private router: Router) { }
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
+  openDialog(isSignIn) {
+    let dialogRef = this.dialog.open(SignInComponent, { panelClass: 'custom-dialog-container', data: { isSignIn: isSignIn } });
+    dialogRef.afterClosed().subscribe(result => {
+      this.signIn();
+    });
+  }
+  signIn() {
+    this.hasSignedIn = true;
+    this.onSignIn.emit(this.hasSignedIn);
+    this.router.navigate(['profile']);
+  }
+  public socialSignIn(socialPlatform: string) {
+    let socialPlatformProvider;
+    if (socialPlatform == "facebook") {
+      socialPlatformProvider = FacebookLoginProvider.PROVIDER_ID;
+    }
+    else if (socialPlatform == "google") {
+      socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
+    }
+
+
+    this.socialAuthService.signIn(socialPlatformProvider).then(
+      (userData) => {
+        console.log(socialPlatform + " sign in data : ", userData);
+        // Now sign-in with userData
+        // ...
+
+      }
     );
-
-  constructor(private breakpointObserver: BreakpointObserver) {}
-
+  }
 }
