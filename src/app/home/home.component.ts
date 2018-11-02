ac0c4b2533d6d5ba16a594256b20542c06d67d7f
Soham Sahare <sohamsahare@gmail.com>
2018-11-02 15:49:17 +0530

    Home Component Edit ->Moved App component homepage to Home Component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {
-  AuthService,
-  FacebookLoginProvider,
-  GoogleLoginProvider
-} from 'angular-6-social-login';
+import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,29 +7,5 @@ import {
 })
 export class HomeComponent  {
   content = [1, 2, 3, 4]
-  title = 'PotentiOMeter';
-  navbarOpen = false;
-  constructor(private socialAuthService: AuthService) { }
-  toggleNavbar() {
-    this.navbarOpen = !this.navbarOpen;
-  }
-  public socialSignIn(socialPlatform: string) {
-    let socialPlatformProvider;
-    if (socialPlatform == "facebook") {
-      socialPlatformProvider = FacebookLoginProvider.PROVIDER_ID;
-    }
-    else if (socialPlatform == "google") {
-      socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
-    }
-
-
-    this.socialAuthService.signIn(socialPlatformProvider).then(
-      (userData) => {
-        console.log(socialPlatform + " sign in data : ", userData);
-        // Now sign-in with userData
-        // ...
-
-      }
-    );
-  }
+ 
 }
