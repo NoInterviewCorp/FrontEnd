c3109a44816ec07f20d9f979af69b9f3f5694d9e
swathi3260 <swathi.peesapati@cgi.com>
2018-11-15 11:31:20 +0530

    card

--- src/models/learningplan.model.ts
+++ src/models/learningplan.model.ts
@@ -1,11 +1,12 @@
+import { Topic } from "./topic.model";
+
 export class LearningPlan{
     LearningPlanId:number;
     Technology:string;
     Summary:string;
     UpVotes:number;
     DownVotes:number;
-    Resources:string[];
     Owner:string;
     Avatar:string;
-    Concepts:string[];
+    Topics:Topic[];
 }
---
+++ src/models/topic.model.ts
@@ -0,0 +1,7 @@
+import { Resource } from "./resource.model";
+
+export class Topic{
+    TopicId:string;
+    Name:string;
+    Resources:Resource[];
+}
